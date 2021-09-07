f17dec0028467098a1e30d00ab14698b8b1c92b2
Jaime Teixidó || MoONStter Teixi <jteixidof@gmail.com>
2021-09-07 22:07:59 +0200

    F

--- Managers/CurrencyManager.js
+++ Managers/CurrencyManager.js
@@ -4,10 +4,11 @@ const MongoDB = require('../Connectors/MongoConnector');
 const collection_data = "data";
 
 module.exports.spend = async function(userId, price, on, referNickName){
-    const referUser = User.getUserDataByName(referNickName);
+    /*const referUser = User.getUserDataByName(referNickName);
     const refer = ((price/global.PARAMS.CONTENT_PREATOR_PERCETNAGE) * 100).toFixed(global.PARAMS.TOKEN_DECIMALS);
     price -= refer;
     const burn = ((price/global.PARAMS.TOKEN_TO_BURN) * 100).toFixed(global.PARAMS.TOKEN_DECIMALS);
     const boss = ((price/global.PARAMS.TOKEN_TO_BOSS) * 100).toFixed(global.PARAMS.TOKEN_DECIMALS);
     const profit = ((price/global.PARAMS.TOKEN_TO_US) * 100).toFixed(global.PARAMS.TOKEN_DECIMALS);
+    */
 }
